93215edbcf3ea7d9d198714101ac9a650f50a7fc
usamabadarr <usamabadarr@gmail.com>
2024-08-21 17:34:59 -0500

    changed postgres password and updated .env file

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,54 @@
 -- schema.sql
 
--- DROP DATABASE IF EXISTS employee_tracker_db;
--- CREATE DATABASE employee_tracker_db;
+-- Drop the database if it exists and create a new one
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
--- \c employees_tracker_db;
+-- Connect to the newly created database
+\c employee_tracker_db
 
+-- Create the department table
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- Create the role table
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- Create the employee table
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id) ON DELETE SET NULL
+);
+
+
+
+
+
+
+-- -- Drop the database if it exists and create a new one
+-- DROP DATABASE IF EXISTS employees_tracker_db;
+-- CREATE DATABASE employees_tracker_db;
+
+-- -- Connect to the newly created database
+-- \c employees_tracker_db
+
+-- -- Create tables
 -- CREATE TABLE department (
 --     id SERIAL PRIMARY KEY,
 --     name VARCHAR(30) UNIQUE NOT NULL
@@ -26,49 +70,9 @@
 --     role_id INTEGER NOT NULL,
 --     manager_id INTEGER,
 --     FOREIGN KEY (role_id) 
---     REFERENCES role(id), 
+--     REFERENCES role(id) 
 --     ON DELETE SET NULL,
 --     FOREIGN KEY (manager_id) 
 --     REFERENCES employee(id) 
 --     ON DELETE SET NULL
 -- );
-
-
-
-
-
--- Drop the database if it exists and create a new one
-DROP DATABASE IF EXISTS employees_tracker_db;
-CREATE DATABASE employees_tracker_db;
-
--- Connect to the newly created database
-\c employees_tracker_db
-
--- Create tables
-CREATE TABLE department (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(30) UNIQUE NOT NULL
-);
-
-CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(30) UNIQUE NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL,
-    FOREIGN KEY (department_id) 
-    REFERENCES department(id) ON DELETE SET NULL
-);
-
-CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER,
-    FOREIGN KEY (role_id) 
-    REFERENCES role(id) 
-    ON DELETE SET NULL,
-    FOREIGN KEY (manager_id) 
-    REFERENCES employee(id) 
-    ON DELETE SET NULL
-);
